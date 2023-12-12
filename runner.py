18702b9c43c50a2ff31aa00ff92f9e85b76e7429
Vyacheslav Kostrov <slavkostrov@gmail.com>
2023-12-12 21:50:40 +0000

    [ADD] Basic hydra usage

--- runner.py
+++ runner.py
@@ -4,5 +4,6 @@ import fire
 from src.infer import infer  # noqa: F401
 from src.train import train  # noqa: F401
 
+# TODO: rewrite to pure argparse, it seems this might be more flexible and safe
 if __name__ == "__main__":
     fire.Fire()
